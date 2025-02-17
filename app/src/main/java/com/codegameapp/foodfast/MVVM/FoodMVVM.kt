7a3bd338ab50fa55e5e3d5e6278097a01221dc5b
Hamza <hamzh9052@gmail.com>
2025-02-17 01:44:07 +0100

    update

--- app/src/main/java/com/codegameapp/foodfast/MVVM/FoodMVVM.kt
+++ app/src/main/java/com/codegameapp/foodfast/MVVM/FoodMVVM.kt
@@ -1,17 +1,22 @@
 package com.codegameapp.foodfast.MVVM
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.codegameapp.foodfast.API.ApiFood
 import com.codegameapp.foodfast.Data.DataFood
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import okhttp3.internal.wait
+import javax.inject.Inject
 
-class FoodMVVM : ViewModel() {
+@HiltViewModel
+class FoodMVVM @Inject constructor(application: Application)  : ViewModel() {
 
 
     private val _listFood = MutableLiveData<List<DataFood>>(mutableListOf<DataFood>())
@@ -31,4 +36,13 @@ class FoodMVVM : ViewModel() {
 
     }
 
+}
+class ViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(FoodMVVM::class.java)) {
+            FoodMVVM(application) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
