39866f0e9142fd8bd420430c92b1fe413c1436cb
Hamza <hamzh9052@gmail.com>
2025-02-17 22:09:33 +0100

    update

--- app/src/main/java/com/codegameapp/foodfast/MainActivity.kt
+++ app/src/main/java/com/codegameapp/foodfast/MainActivity.kt
@@ -3,6 +3,7 @@ package com.codegameapp.foodfast
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -28,6 +29,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.PlusOne
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -39,14 +41,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.codegameapp.foodfast.BottomBar.BottomBarNav
 import com.codegameapp.foodfast.Data.DataFood
 import com.codegameapp.foodfast.MVVM.FoodMVVM
 import com.codegameapp.foodfast.MVVM.ViewModelFactory
 import com.codegameapp.foodfast.Screen.HomeScreen
+import com.codegameapp.foodfast.Screen.LoginScreen
+import com.codegameapp.foodfast.Screen.NewAccountScreen
 import com.codegameapp.foodfast.Screen.ProductScreen
 import com.codegameapp.foodfast.Screen.Screen
+import com.codegameapp.foodfast.Screen.Screen_
 import com.codegameapp.foodfast.ui.MyAppTheme
 import kotlinx.serialization.json.Json
 import java.net.URLDecoder
@@ -64,7 +70,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             actionBar?.hide()
 
-            MyAppTheme(){
+            MyAppTheme() {
                 val navController = rememberNavController()
                 Scaffold(
                     contentWindowInsets = WindowInsets.safeDrawing,
@@ -72,54 +78,82 @@ class MainActivity : AppCompatActivity() {
                         .fillMaxSize()
                         .fillMaxWidth(),
                     bottomBar = {
-                        BottomAppBar(
-                            modifier = Modifier
-                                .height(65.dp)
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp)),
-                            cutoutShape = CircleShape,
-                            backgroundColor = colorResource(R.color.Original)
-                        ) {
-                            BottomBarNav(navController = navController)
+                        val currentBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentRoute = currentBackStackEntry?.destination?.route ?: ""
+                        if (
+                            currentRoute.startsWith("login") ||
+                            currentRoute.startsWith("new_account") ||
+                            currentRoute.startsWith("product")
+                            ) {
+                            Log.e("Screen", "I'm on the correct screen")
+                        } else {
+                            Log.e("Screen", "I'm on a different screen")
+                            BottomAppBar(
+                                modifier = Modifier
+                                    .height(65.dp)
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp)),
+                                cutoutShape = CircleShape,
+                                backgroundColor = colorResource(R.color.Original)
+                            ) {
+                                BottomBarNav(navController = navController)
+                            }
                         }
+
                     },
                     floatingActionButtonPosition = FabPosition.Center,
                     isFloatingActionButtonDocked = true,
                     floatingActionButton = {
-                        FloatingActionButton(
-                            elevation =FloatingActionButtonDefaults.elevation(12.dp) ,
-                            shape = CircleShape,
-                            onClick = {
-                                //this onClick is for add some product
-                            },
-                            backgroundColor = colorResource(R.color.Original)
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.Add,
-                                contentDescription = "Add icon",
-                                tint = Color.White
-                            )
+                        val currentBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentRoute = currentBackStackEntry?.destination?.route ?: ""
+                        if (
+                            currentRoute.startsWith("login") ||
+                            currentRoute.startsWith("new_account") ||
+                            currentRoute.startsWith("product")
+                            ) {
+                            Log.e("Screen", "I'm on the correct screen")
+                        } else {
+                            FloatingActionButton(
+                                elevation = FloatingActionButtonDefaults.elevation(12.dp),
+                                shape = CircleShape,
+                                onClick = {
+                                    //this onClick is for add some product
+                                },
+                                backgroundColor = colorResource(R.color.Original)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Add,
+                                    contentDescription = "Add icon",
+                                    tint = Color.White
+                                )
+                            }
                         }
                     }
-                ) {padding->
+                ) { padding ->
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .fillMaxWidth()
                             .safeDrawingPadding()
                     ) {
-                        NavHost(navController, startDestination = Screen.Home.route!!) {
+                        NavHost(navController, startDestination = Screen_.Login.route) {
 
+                            composable(Screen_.Login.route) {
+                                LoginScreen(navController)
+                            }
+                            composable(Screen_.Acc.route) {
+                                NewAccountScreen(navController)
+                            }
                             //profile
-                            composable(Screen.Home.route) {
-                                HomeScreen(MVVM,navController)
+                            composable(Screen.Home.route!!) {
+                                HomeScreen(MVVM, navController)
                             }
                             //pickUp
                             composable(Screen.Profile.route!!) {
                                 // PickupScreen()
                             }
                             composable("product/{jsonData}")
-                            {backStackEntry->
+                            { backStackEntry ->
                                 val jsonData = backStackEntry.arguments?.getString("jsonData") ?: ""
                                 val decodedJson = Uri.decode(jsonData)
                                 val dataProduct = Json.decodeFromString<DataFood>(decodedJson)
